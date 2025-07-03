6c20a3e23085637ab6371236f59e8c5d32791e43
CreativeSelf1 <fernacastillo.perez0607@gmail.com>
2025-07-03 19:30:55 -0400

    solo archivos

--- backend/backend2/src/controllers/ChecksController.js
+++
@@ -1,5 +0,0 @@
-export function checkAdmin(req, res) {
-  res.status(200).json({
-    message: 'Acceso permitido',
-  });
-}
--- backend/backend2/src/middlewares/isAdmin.js
+++
@@ -1,6 +0,0 @@
-export function isAdminMiddleware(req, res, next) {
-  if (!req.user?.isAdmin) {
-    return res.status(403).json({ message: 'Acceso denegado: solo para administradores' });
-  }
-  next();
-}
